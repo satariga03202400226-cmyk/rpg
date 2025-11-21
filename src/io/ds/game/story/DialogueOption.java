823619363c88318e348406b26016ab096cf8cb03
sheynah <satariga03202400226@usep.edu.ph>
2025-11-22 00:25:17 +0800

    DialogueOption.java

--- src/io/ds/game/story/DialogueOption.java
+++ src/io/ds/game/story/DialogueOption.java
@@ -1,5 +1,18 @@
 package game.story;
 
 public class DialogueOption {
-    
+    private String text;
+    private Dialogue nextDialogue;
+
+    public DialogueOption(String text, Dialogue nextDialogue) {
+        this.text = text;
+        this.nextDialogue = nextDialogue;
+    }
+    public String getText() {
+        return text;
+    }
+    public Dialogue getNextDialogue() {
+        return nextDialogue;
+    }
 }
+
