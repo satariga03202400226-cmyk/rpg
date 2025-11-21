1afa4884aa3a4e9916cbf3ab3a9c8301a4178164
satariga03202400226-cmyk <satariga03202400226@usep.edu.ph>
2025-11-21 23:02:34 +0800

    Dialogue.java

--- src/io/ds/game/story/Dialogue.java
+++ src/io/ds/game/story/Dialogue.java
@@ -1,5 +1,13 @@
 package game.story;
 
 public class Dialogue {
- 
+  private String dialogueID;
+    private String text;
+    private Dialogue nextDialogue;
+
+    public Dialogue(String dialogueID, String text, DialogueType type) {
+        this.dialogueID = dialogueID;
+        this.text = text;
+        this.type = type;
+    }
 }
