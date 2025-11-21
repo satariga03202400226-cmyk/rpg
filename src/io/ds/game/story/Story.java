9f05694b97bfb2e7df9a7e197d976f70249448cc
sheynah <satariga03202400226@usep.edu.ph>
2025-11-22 00:31:52 +0800

    Story.java

--- src/io/ds/game/story/Story.java
+++ src/io/ds/game/story/Story.java
@@ -1,5 +1,53 @@
 package game.story;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Story {
-    
+    private Queue<Dialogue> dialogueQueue = new LinkedList<>();
+    private Dialogue currentDialogue;
+
+    public void addDialogue(Dialogue dialogue) {
+        dialogueQueue.offer(dialogue);
+    }
+    public void startStory() {
+        currentDialogue = dialogueQueue.poll();
+    }
+    public Dialogue getCurrentDialogue() {
+        return currentDialogue;
+    }
+    public void nextDialogue() {
+        if (currentDialogue != null) {
+            if (currentDialogue.getNextDialogue() != null) {
+                currentDialogue = currentDialogue.getNextDialogue();
+            } else {
+                currentDialogue = dialogueQueue.poll();
+            }
+        }
+    }
+    public void chooseOption(int index) {
+        if (currentDialogue.getOptions() != null && index >= 0 && index < currentDialogue.getOptions().length) {
+            currentDialogue = currentDialogue.getOptions()[index].getNextDialogue();
+        }
+    }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        Story story = new Story();
+        Dialogue intro = new Dialogue("scene-1","Mao ni first scene...", Type.DIALOG);
+
+        Dialogue choiceScene = new Dialogue("scene-2","Tas next... asa ka muadto?", Type.CHOICE);
+        DialogueOption opt1 = new DialogueOption("Sa left!", null);
+        DialogueOption opt2 = new DialogueOption("Sa right!", null);
+        choiceScene.setOptions(new DialogueOption[]{opt1, opt2});
+        intro.setNextDialogue(choiceScene);
+        story.addDialogue(intro);
+        story.addDialogue(choiceScene);
+        story.startStory();
+        System.out.println(story.getCurrentDialogue().getText());
+        story.nextDialogue();
+        System.out.println(story.getCurrentDialogue().getText());
+    }
+}
+
