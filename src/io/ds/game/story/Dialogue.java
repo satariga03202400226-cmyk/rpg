f804b6a46b4b684c091a559d17b96c15b2ee984d
sheynah <satariga03202400226@usep.edu.ph>
2025-11-22 00:23:18 +0800

    Dialogue.java

--- src/io/ds/game/story/Dialogue.java
+++ src/io/ds/game/story/Dialogue.java
@@ -1,13 +1,51 @@
 package game.story;
 
-public class Dialogue {
-  private String dialogueID;
-    private String text;
-    private Dialogue nextDialogue;
+public enum Type {
+    DIALOG,
+    CHOICE,
+    BATTLE_SCENE_TRIGGER
+}
 
-    public Dialogue(String dialogueID, String text, DialogueType type) {
+public class Dialogue {
+    private String dialogueID;
+    private String text;
+    private DialogueOption[] options;
+    private Dialogue nextDialogue;
+    private Type type;
+
+    public Dialogue(String dialogueID, String text, Type type) {
         this.dialogueID = dialogueID;
         this.text = text;
         this.type = type;
     }
+
+    public void setOptions(DialogueOption[] options) {
+        this.options = options;
+    }
+
+    public void setNextDialogue(Dialogue nextDialogue) {
+        this.nextDialogue = nextDialogue;
+    }
+
+    public String getDialogueID() {
+        return dialogueID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public DialogueOption[] getOptions() {
+        return options;
+    }
+
+    public Dialogue getNextDialogue() {
+        return nextDialogue;
+    }
+
+    public Type getType() {
+        return type;
+    }
 }
+
+
