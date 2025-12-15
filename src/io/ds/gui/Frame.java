c3f77f2d079b301973ba011f2121196abb6d4a0d
sheynah <satariga03202400226@usep.edu.ph>
2025-12-15 15:05:09 +0800

    Update Frame.java

--- src/io/ds/gui/Frame.java
+++ src/io/ds/gui/Frame.java
@@ -1,5 +1,28 @@
 package gui;
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Frame {
-    
+public class Frame extends JFrame {
+
+    private final JPanel contentPanel;
+
+    public Frame() {
+        super("Knights Requiem");
+        setSize(816, 639);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        contentPanel = new JPanel(new CardLayout());
+        setContentPane(contentPanel);
+    }
+
+    public JPanel getContentPanel() {
+        return contentPanel;
+    }
+
+    public CardLayout getCardLayout() {
+        return (CardLayout) contentPanel.getLayout();
+    }
 }
